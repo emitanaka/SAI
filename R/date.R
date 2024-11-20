03c3303d44a96a38e84e98e1026c575278abef6f
Emi Tanaka <dr.emi.tanaka@gmail.com>
2024-11-20 15:44:23 +1100

    update doc for sai_clean_date

--- R/date.R
+++ R/date.R
@@ -4,18 +4,18 @@
 #'
 #' @param dates_vector A character vector that is assumed to be dates.
 #' @param input_format A character value to specify input date format.
-#' @param output_format A character value to specify output date format.
+#' @param ... Extra options for the large language model.
 #'
 #' @export
-sai_clean_date <- function(dates_vector, input_format = "DD/MM/YYYY", output_format = "YYYY-MM-DD", copy = FALSE, ...) {
+sai_clean_date <- function(dates_vector, input_format = "DD/MM/YYYY", copy = FALSE, ...) {
   out <- sai_assist(
     c(list("The input data is a vector of dates, the input dates are in format of {input_format*}.
             InputData = {dates_vector*}.
             Input data are in different date format.
-            You need to convert the input date into {output_format*}.
+            You need to convert the input date into YYYY-MM-DD.
             Only return the output dates in a vector format.
             If the input is not date, return NA.")),
-    format = "json")
+    format = "json", ...)
   if(copy) clipr::write_clip(paste0(deparse(out), collapse = ""))
   as.Date(out[[1]])
 }
